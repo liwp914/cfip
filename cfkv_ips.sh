c138caf51c5b496c726a68d4c3221025580f6835
liwp914 <2127780771@qq.com>
2025-01-11 15:09:57 +0800

    Update cfkv_ips.sh

--- cfkv_ips.sh
+++ cfkv_ips.sh
@@ -10,20 +10,22 @@ files=("$@")
 # 循环处理每个文件
 for FILENAME in "${files[@]}"; do
     # 判断文件是否存在，如果不存在则跳过该文件的处理
-    if [! -f "$FILENAME" ]; then
+    if [ ! -f "$FILENAME" ]; then
         echo "文件 $FILENAME 不存在，跳过该文件的处理。"
         continue
     fi
 
     BASE64_TEXT=$(head -n 65 "$FILENAME" | base64 -w 0)
-    URL="https://${CF_DOMAIN}/${FILENAME}?token=${CF_TOKEN}&b64=${BASE64_TEXT}"
+    FILENAME_URL=$(urlencode "$FILENAME")
+    URL="https://${CF_DOMAIN}/${FILENAME_URL}?token=${CF_TOKEN}&b64=${BASE64_TEXT}"
 
     # 使用 curl 发送请求
-    curl -s "$URL" &
-
-    echo "已处理文件 $FILENAME 的数据更新，继续处理下一个文件..."
+    curl -s "$URL" &> /dev/null
+    if [ $? -ne 0 ]; then
+        echo "文件 $FILENAME 的数据更新失败。"
+    else
+        echo "已处理文件 $FILENAME 的数据更新，继续处理下一个文件..."
+    fi
 done
 
-echo "所有文件的数据更新完成,倒数5秒后自动关闭窗口..."
-sleep 5
-exit
+echo "所有文件的数据更新完成。"
